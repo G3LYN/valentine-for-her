4c5cde37452069beeab552e58b419c32eb20fac4
Georgelyn <136056481+G3LYN@users.noreply.github.com>
2026-02-14 16:42:58 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("startBtn");
-const scene = document.getElementById("scene");
-
-btn.onclick = () => {
-    btn.style.display = "none";
-    scene.classList.remove("hidden");
-};
